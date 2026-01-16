e4966214d3fc4a217c8ed2bb6c7d0bfc154d15fa
Jayslen <jayslen134@gmail.com>
2026-01-15 23:48:29 -0400

    update: Modify the insertion values function to insert the data by chunks to avoid overflow

--- src/scraper/Insertion/helpers/dbQuery.ts
+++ src/scraper/Insertion/helpers/dbQuery.ts
@@ -2,6 +2,8 @@ import path from 'node:path'
 import fs from 'node:fs/promises'
 import db from '../../dbInstance.js'
 
+const ROWS_PER_INSERT = 1000
+
 export async function insertValues(
   table: string,
   columns: string[],
@@ -9,24 +11,31 @@ export async function insertValues(
   entity: string,
   msg?: string
 ) {
-  const query = generateQuery(table, columns, values)
-  try {
-    const results = await db.unsafe(query)
-    if (results.affectedRows > 0) {
-      console.log(msg ?? `Inserted ${results.affectedRows} rows into ${entity}`)
-    } else {
-      console.log(`No rows inserted into ${entity}`)
+  let rowsInserted = 0
+
+  for (let i = 0; i <= values.length; i += ROWS_PER_INSERT) {
+    const rows = values.slice(i, i + ROWS_PER_INSERT)
+    const query = generateQuery(table, columns, rows)
+    try {
+      const results = await db.unsafe(query)
+
+      if (!results.affectedRows) {
+        console.log(`No rows inserted into ${entity}`)
+        break
+      }
+      rowsInserted += results.affectedRows
+    } catch (error: any) {
+      const errorMsg = `Error inserting ${entity}: ${error.message}`
+      const errorsPath = path.join(process.cwd(), '/debug/logs-errors')
+      await fs.mkdir(errorsPath, { recursive: true })
+      console.error(errorMsg)
+      await Bun.write(
+        path.join(errorsPath, `error-${entity}.txt`),
+        `${errorMsg}\n${query}`
+      )
     }
-  } catch (error: any) {
-    const errorMsg = `Error inserting ${entity}: ${error.message}`
-    const errorsPath = path.join(process.cwd(), '/debug/logs-errors')
-    await fs.mkdir(errorsPath, { recursive: true })
-    console.error(errorMsg)
-    await Bun.write(
-      path.join(errorsPath, `error-${entity}.txt`),
-      `${errorMsg}\n${query}`
-    )
   }
+  console.log(msg ?? `Inserted ${rowsInserted} rows into ${entity}`)
 }
 
 function generateQuery(
